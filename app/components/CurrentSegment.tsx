9dc4f49f9b229b7e4558ab020b92bf24a81d3159
Harrison Liddiard <omniaura5@gmail.com>
2024-12-18 21:54:54 -0700

    reimplement progress bar using <div>s

--- app/components/CurrentSegment.tsx
+++ app/components/CurrentSegment.tsx
@@ -2,6 +2,7 @@ import { TrainStatus } from '@/app/types'
 import cn from 'classnames'
 import { getCurrentSegmentProgress } from '../utils'
 import Progress from './Progress'
+import { useEffect, useRef } from 'react'
 
 interface CurrentSegmentProps {
   trainStatus: TrainStatus
@@ -15,10 +16,20 @@ const formatMinutes = (minutes: number) => {
 }
 
 const CurrentSegment = ({ trainStatus }: CurrentSegmentProps) => {
+  const progressValueRef = useRef<HTMLDivElement>(null)
+
   const { minsToDeparture, minsToArrival, percent } =
     getCurrentSegmentProgress(trainStatus)
   const { prevStation, curStation, nextStation } = trainStatus
   const isDeparting = !!minsToDeparture
+
+  // when the stations change, cancel the previous animation so it doesn't
+  // animate slowly back from 100% to 0%
+  useEffect(() => {
+    if (progressValueRef.current) {
+      progressValueRef.current.getAnimations()[0].cancel()
+    }
+  }, [prevStation?.code, nextStation?.code, curStation?.code])
 
   let segmentStartStation
   let segmentEndStation
@@ -48,9 +59,11 @@ const CurrentSegment = ({ trainStatus }: CurrentSegmentProps) => {
         <span className="text-right">{segmentEndStation?.name}</span>
       </div>
 
-      <div className="relative h-4">
-        <Progress percent={percent} id="segmentProgress" />
-      </div>
+      <Progress
+        percent={percent}
+        id="segmentProgress"
+        progressValueRef={progressValueRef}
+      />
       <label
         htmlFor="segmentProgress"
         className={cn('block', { 'text-right': !isDeparting })}
--- app/components/Progress.tsx
+++ app/components/Progress.tsx
@@ -1,15 +1,47 @@
 import cn from 'classnames'
+import { useEffect, useState } from 'react'
 
-const Progress = ({ percent = 0, className = '', id = '' }) => (
-  <progress
-    id={id}
-    value={percent}
-    max={1}
-    className={cn(
-      className,
-      'relative flex items-center p-[0.15rem] w-full progress-unfilled:rounded-full progress-unfilled:appearance-none h-4 progress-unfilled:bg-positron-gray-200 progress-filled:bg-amtrak-blue-500 progress-filled:absolute progress-filled:left-0 progress-filled:top-0 progress-filled:min-w-4 progress-filled:rounded-full progress-filled:transition-all progress-filled:duration-[15s] progress-filled:ease-linear before-after:content-[""] before-after:block before-after:z-10 before-after:bg-white before-after:w-3 before-after:aspect-square before-after:shrink-0 before-after:rounded-full before-after:top-0 before:left-1 after:right-1',
-    )}
-  />
-)
+const Progress = ({
+  percent = 0,
+  className = '',
+  id = '',
+  progressValueRef,
+}: {
+  percent: number
+  className?: string
+  id?: string
+  progressValueRef?: React.RefObject<HTMLDivElement>
+}) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    // prevent hydration error with progress bar width not exactly matching
+    // in the time it takes to be rendered between client and server
+    return null
+  }
+
+  return (
+    <div
+      id={id}
+      className={cn(
+        className,
+        'relative flex items-center justify-between p-[0.15rem] w-full rounded-full appearance-none h-4 bg-positron-gray-200 before-after:content-[""] before-after:block before-after:z-10 before-after:bg-white before-after:w-3 before-after:aspect-square before-after:shrink-0 before-after:rounded-full before-after:top-0 before:left-1 after:right-1',
+      )}
+    >
+      <div
+        style={{
+          width: `${Math.min(percent, 1) * 100}%`,
+          minWidth: '1rem',
+        }}
+        className="absolute left-0 top-0 h-full rounded-full transition-all duration-[15s] ease-linear bg-amtrak-blue-500"
+        ref={progressValueRef}
+      />
+    </div>
+  )
+}
 
 export default Progress
