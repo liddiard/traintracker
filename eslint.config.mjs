3e73d2e031077d54c9bc80545fec683151b10af0
Harrison Liddiard <git@harrisonliddiard.com>
2026-01-19 20:08:17 -0500

    refactor(lint): add (commented) rule to check for unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,13 @@
 import nextPlugin from 'eslint-config-next'
 
-const eslintConfig = [...nextPlugin, { ignores: ['db/generated/'] }]
+const eslintConfig = [
+  ...nextPlugin,
+  {
+    rules: {
+      // 'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+  { ignores: ['db/generated/'] },
+]
 
 export default eslintConfig
