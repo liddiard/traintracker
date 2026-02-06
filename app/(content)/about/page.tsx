9bebc009d34b7ed7b3d3859456c13fe80e7660eb
Harrison Liddiard <git@harrisonliddiard.com>
2026-02-06 16:37:53 -0500

    content(about): add component for about page

---
+++ app/(content)/about/page.tsx
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { marked } from 'marked'
+
+// Recreate __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default async function About() {
+  const filePath = path.join(__dirname, 'page.md')
+  const markdown = fs.readFileSync(filePath, 'utf-8')
+
+  const html = marked.parse(markdown)
+
+  return (
+    <article className="prose dark:prose-invert">
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </article>
+  )
+}
